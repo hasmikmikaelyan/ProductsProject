fe2bf03ac6b781cefc6be69035ffb0faabefc215
hasmikmikaelyan <hasmik.mikaelyan.hm@gmail.com>
2016-06-13 01:51:37 +0400

    Add project files.

---
+++ ProductsProject/Controllers/ProductsController.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProductsProject.Models;
+using PagedList;
+
+namespace ProductsProject.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [AllowAnonymous]
+        // GET: Products
+        public async Task<ActionResult> Index(string order, string search, string currentFilter, int? page)
+        {
+            ViewBag.CurrentSort = order;
+
+            if (search != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                search = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = search;
+
+            var products = await db.Products.ToListAsync();
+
+            if (!String.IsNullOrEmpty(search))
+            {
+                products = products.Where(s => s.Name.Contains(search)).ToList();
+            }
+
+            switch (order)
+            {
+                case "name_desc":
+                    products = products.OrderByDescending(x => x.Name).ToList();
+                    break;
+                case "price_asc":
+                    products = products.OrderBy(x => x.Price).ToList();
+                    break;
+                case "price_desc":
+                    products = products.OrderByDescending(x => x.Price).ToList();
+                    break;
+                default:
+                    products = products.OrderBy(x => x.Name).ToList();
+                    break;
+            }
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+
+            return View(products.ToPagedList(pageNumber, pageSize));
+        }
+
+        [AllowAnonymous]
+        // GET: Products/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Name,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                product.Id = Guid.NewGuid().ToString();
+                db.Products.Add(product);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Price")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = await db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            Product product = await db.Products.FindAsync(id);
+            db.Products.Remove(product);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<ActionResult> Random(string quan)
+        {
+            int quantity = Convert.ToInt32(quan);
+            if (quantity < 0)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Content("Quantity should be positive");
+            }
+
+            for (int i = 1; i <= quantity; i++)
+            {
+                db.Products.Add(new Product
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = $"Product {i}",
+                    Price = i * 10
+                });
+            }
+
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
